baa2b0fbab4c42e83aacc210f5e352edd8c65770
hb57786 <tadeufcad@gmail.com>
2021-08-14 07:57:27 -0300

    Adding firebase login error messages

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -1,17 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
+import 'package:loja_virtual/helpers/firebase_errors.dart';
 import 'package:loja_virtual/models/user.dart';
 
 class UserManager {
   final FirebaseAuth auth = FirebaseAuth.instance;
 
-  Future<void> signIn(UserData user) async {
+  Future<void> signIn(
+      {UserData user, Function onFail, Function onSuccess}) async {
     try {
       final UserCredential result = await auth.signInWithEmailAndPassword(
           email: user.email, password: user.password);
-      print(result.user.uid);
-    } on PlatformException catch (e) {
-      print(e);
+      onSuccess();
+    } on FirebaseAuthException catch (e) {
+      onFail(getErrorString(e.code));
     }
   }
 }
