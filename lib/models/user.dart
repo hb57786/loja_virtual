ba29fb362e8d13564ec2a59baddba99bde05d5d9
hb57786 <tadeufcad@gmail.com>
2021-08-20 07:53:16 -0300

    Saving User Data in Firebase

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,8 +1,25 @@
-class UserData {
-  UserData({this.email, this.password, this.name});
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class UserData {
+  UserData({this.email, this.password, this.name, this.id});
+
+  String id;
   String name;
   String email;
   String password;
   String confirmPassword;
+
+  DocumentReference get firestoreRef =>
+      FirebaseFirestore.instance.doc('users/$id');
+
+  Future<void> saveData() async {
+    await firestoreRef.set(toMap());
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'email': email,
+    };
+  }
 }
